4e3cc4c39bac3477191639d4af4ac822986afe72
thuraaung-ppt <thura.aung61296@gmail.com>
2018-08-24 13:34:56 +0630

    Modified test.py for testing

--- test.py
+++ test.py
@@ -2,22 +2,43 @@
 import unittest 
 from main import app
 
-class ExampleTest(unittest.TestCase):
+
+class Blueprint_Test(unittest.TestCase):
 
 	def setUp(self):
-		self.app = app.test_client() 
-
-	def tearDown(self):
-		pass 
+		self.app = app.test_client()
 
 	def test_homepage(self):
 		response = self.app.get("/")
 		self.assertEqual(response.status_code,200)
 
-	def test_detail(self):	
+	def test_about(self):	
 		response = self.app.get("/about")
 		self.assertEqual(response.status_code,200)
 
+	def test_contact(self):
+		response = self.app.get("/contact")
+		self.assertEqual(response.status_code,200)
+
+	def test_bns_leader(self):
+		response = self.app.get("/bns")
+		self.assertEqual(response.status_code,301)
+
+	def test_cdt_parties(self):
+		response = self.app.get("/cdt")
+		self.assertEqual(response.status_code,301)
+
+	def test_cele(self):		
+		response = self.app.get("/cele")
+		self.assertEqual(response.status_code,301)
+
+	def test_mb_pmt(self):
+		response = self.app.get("/mb")
+		self.assertEqual(response.status_code,301)
+
+	def test_modern_figures(self):
+		response = self.app.get("/modern_figures")
+		self.assertEqual(response.status_code,301)
 
 
 if __name__ == '__main__':
